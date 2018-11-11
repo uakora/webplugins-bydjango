950bb0935a33764c533ba4b2bd81a463a604b5a1
uakora <mEnternityevoL@gmail.com>
2018-11-11 23:07:00 +0800

    模态框中显示datatable

--- src/datatables/urls.py
+++ src/datatables/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('demo01', views.demo01),
     path('demo02', views.demo02),
     path('demo03', views.demo03),
+    path('demo04', views.demo04),
 ]
--- src/datatables/views.py
+++ src/datatables/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def demo01(request):
     return render(request, 'demo01.html')
 
+
 def demo02(request):
     return render(request, 'demo02.html')
 
+
 def demo03(request):
     return render(request, 'demo03.html')
+
+
+def demo04(request):
+    return render(request, 'demo04.html')
